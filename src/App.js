9999e734c3c4c738887cb0cde4afbfb1ed791bf8
anai1203 <anai.flor.angelica@gmail.com>
2022-10-27 19:27:09 -0500

    V02 my app 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - ana
+          Learn React - Flor
         </a>
       </header>
     </div>
